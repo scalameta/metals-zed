76096ace410ed2a7d6b4a87c40860e5d891d7e1e
WeetHet <stas.ale66@gmail.com>
2024-10-09 17:14:37 +0300

    Add main function

---
+++ languages/scala/runnables.scm
@@ -0,0 +1,14 @@
+(
+    (
+        (function_definition
+            (annotation
+                name: (type_identifier) @run
+            )
+            name: _
+            parameters: _
+            body: _
+        ) @_scala_main_function_end
+        (#eq? @run "main")
+    )
+    (#set! tag scala-main)
+)
